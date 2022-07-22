b49e6f8506bf1646a93816e2fa6761445a0ff603
jinhyo <sosilion@gmail.com>
2022-07-22 23:25:05 +0900

    2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { getVideoDurationInSeconds } from 'get-video-duration';
 
 @Controller()
 export class AppController {
-  @Get('video-duration')
-  async getVideoDuration(url: string): Promise<number> {
-    const duration = await getVideoDurationInSeconds(
-      'https://a.seoltab.com/00132fe257e64fe690b668a39feb33ec/63e3baeafaa241ec85bd9474ef938919.mp4',
+  @Post('video-duration')
+  async getVideoDuration(@Body('url') url: string): Promise<number> {
+    console.log(
+      '🚀 ~ file: app.controller.ts ~ line 8 ~ AppController ~ getVideoDuration ~ url',
+      url,
     );
+
+    const example =
+      'https://a.seoltab.com/00132fe257e64fe690b668a39feb33ec/63e3baeafaa241ec85bd9474ef938919.mp4';
+    const example2 =
+      'https://video-convert-test.s3.ap-northeast-2.amazonaws.com/3fa281f2b0f344efb1c25ac7c05ac66f/ff134dde0be54a25af1f58a8f7f48c73.mp4';
+    const duration = await getVideoDurationInSeconds(url);
     console.log(duration);
 
     return duration;
